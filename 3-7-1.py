90e49d72c8200c83ed273e9e23316d5e9e1c3059
itc-s24010 <s24010@std.it-college.ac.jp>
2024-07-10 08:04:04 +0000

    7月10日

---
+++ 3-7-1.py
@@ -0,0 +1,21 @@
+with open('sample.txt', 'r') as f:
+    line = f.readline()
+    print(line)
+
+with open('sample.txt', 'r') as f:
+    lines = f.readlines()
+    print(line)
+
+with open('sample.txt', 'r') as f:
+    lines = f.readline()
+    print(line)
+
+with open('sample2.txt','w') as f:
+    f.write('test')
+
+with open('sample2.txt','a') as f:
+    f.write('test2')
+
+with open('sample.txt','r') as f:
+    for line in f:
+        print(line.strip())
