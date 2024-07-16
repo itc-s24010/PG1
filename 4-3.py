b828bbaca6b5fe0f37f1812b6116a7cce58d73b9
itc-s24010 <s24010@std.it-college.ac.jp>
2024-07-16 07:59:06 +0000

    7月16日

---
+++ 4-3.py
@@ -0,0 +1,10 @@
+def show_args(*args):
+    '''与えられた複数の位置引数をタプルにまとめて受け取りそのタプルを表示して返す'''
+    print('positional arguments:', args)
+    return args
+
+def show_kwargs(**kwargs):
+    '''与えられた複数のキーワードを辞書にまとめて受け取りその辞書を表示して返す'''
+    print('Keyword arguments:', kwargs)
+    return kwargs
+        
